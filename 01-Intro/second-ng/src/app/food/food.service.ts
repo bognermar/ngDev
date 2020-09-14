a778e9a617f678f8f2446ae9c77941977cc7669b
bognermar <marlene@bogner.nu>
2020-09-14 14:23:22 +0200

    add assets, install node in foodlist

--- 01-Intro/second-ng/src/app/food/food.service.ts
+++ 01-Intro/second-ng/src/app/food/food.service.ts
@@ -1,19 +1,24 @@
 import { Injectable } from '@angular/core';
 import { of, Observable } from 'rxjs';
 import { FoodItem } from './food-item.model';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FoodService {
-  constructor() {}
+  constructor(private httpClient: HttpClient) {}
 
+  /*Wird mit JSON ersetzt
   food = [
     { name: 'Banane', price: 2 },
     { name: 'Apfel', price: 1 },
-  ];
+  ];*/
 
   getFood(): Observable<FoodItem[]> {
-    return of(this.food); //Mit of wird das ganze zu einem Observable Stream
+    //return of(this.food); //Mit of wird das ganze zu einem Observable Stream
+
+    return this.httpClient.get<FoodItem[]>(environment.url);
   }
 }
